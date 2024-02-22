d9983ff6230192a4e06cf3af0cac301ca7e1c48a
dustinusey <dustinusey@me.com>
2024-02-21 18:16:12 -0600

    added a delete btn

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from "react";
 const App = () => {
   const [tasks, setTasks] = useState([]);
   const [currentValue, setCurrentValue] = useState("");
+
+  function handleDeleteTask(index) {
+    setTasks(
+      tasks.filter((_, i) => {
+        return i !== index;
+      })
+    );
+  }
+
   return (
     <div>
       <form
@@ -21,7 +30,18 @@ const App = () => {
       </form>
       <ul>
         {tasks.map((task, index) => {
-          return <li key={index}>{task}</li>;
+          return (
+            <li key={index}>
+              <p>{task}</p>
+              <button
+                onClick={() => {
+                  handleDeleteTask(index);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
         })}
       </ul>
     </div>
