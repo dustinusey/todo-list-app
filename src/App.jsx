f16de52b9289856e394644720deea880abc883e3
dustinusey <dustinusey@me.com>
2024-02-22 12:45:55 -0600

    finished the todolist styling as per SOLI's demands on stream

--- src/App.jsx
+++ src/App.jsx
@@ -12,38 +12,51 @@ const App = () => {
   }
 
   return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          setTasks([...tasks, currentValue]);
-          setCurrentValue("");
-        }}
-      >
-        <input
-          onChange={(e) => {
-            setCurrentValue(e.target.value);
+    <div className="h-screen grid place-items-center bg-slate-800">
+      <div className="bg-gray-200 p-5 rounded-lg w-[85%] max-w-[500px] m-auto">
+        <form
+          className=""
+          onSubmit={(e) => {
+            e.preventDefault();
+            setTasks([...tasks, currentValue]);
+            setCurrentValue("");
           }}
-          type="text"
-          value={currentValue}
-        />
-      </form>
-      <ul>
-        {tasks.map((task, index) => {
-          return (
-            <li key={index}>
-              <p>{task}</p>
-              <button
-                onClick={() => {
-                  handleDeleteTask(index);
-                }}
-              >
-                Delete
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+        >
+          <label
+            className="text-center font-bold text-lg  mb-5 block text-slate-500"
+            htmlFor="newTask"
+          >
+            To-Do List
+          </label>
+          <input
+            id="newTask"
+            onChange={(e) => {
+              setCurrentValue(e.target.value);
+            }}
+            className="w-full border-2 border-slate-400 rounded-md outline-slate-500 px-5 py-3"
+            type="text"
+            value={currentValue}
+            placeholder="Add a new task"
+          />
+        </form>
+        <ul className="p-2">
+          {tasks.map((task, index) => {
+            return (
+              <li className="flex place-items-center mt-2" key={index}>
+                <p className="text-slate-500">{task}</p>
+                <button
+                  className="ml-auto bg-red-700 text-red-200 border-2 border-red-500 px-3 py-1 rounded-md"
+                  onClick={() => {
+                    handleDeleteTask(index);
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
